2dcdcba535b20e2868a4c098d289b445a9d02f10
Arun Mathew <amathew5@mail.yu.edu>
2023-11-28 05:01:19 -0500

    project 3

---
+++ Analysis of death by selected causes from 2014 to 2019/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="src",
+    version=0.1,
+    description="Analysis of death by selected causes from 2014 to 2019",
+    author="Arun Mathew",
+    packages=["data_summary", "EDA", "inference"],
+    zip_safe=False,
+)
